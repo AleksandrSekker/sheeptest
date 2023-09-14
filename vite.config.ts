d72c386d4b1ee10c4f294a0b798c583dbda2dbf6
aleksandrsekker <oleksandr.sekker@onix-systems.com>
2023-09-14 12:43:46 +0300

    deploying to GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
+const repositoryName = 'sheeptest'; // Change this to your repository name
+const base =
+  process.env.NODE_ENV === 'production' ? `/${repositoryName}/` : '/';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/sheeptest/',
+  base: base,
   test: {
     environment: 'happy-dom',
   },
